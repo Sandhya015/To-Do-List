2395489f70c82a85f0b1c766e1cafeab183b86d5
Sandhya <144054124+sandhya-1234@users.noreply.github.com>
2024-01-20 18:58:36 +0530

    Add files via upload

---
+++ todolist/Ascript.js
@@ -0,0 +1,38 @@
+var app = angular.module('myApp', []);
+app.controller('myController', function ($scope) {
+
+$scope.tasks = [
+{ name: 'Attend Class'},
+{ name: 'Complete Assignment'},
+{ name: 'Study for CIE'}
+];
+$scope.newTaskName = ' ';
+$scope.editingTask = null;
+$scope.editedTaskName = ' ';
+$scope.addTask = function () {
+if ($scope.newTaskName) {
+$scope.tasks.push({ name: $scope.newTaskName });
+$scope.newTaskName = ' ';
+}
+};
+$scope.editTask = function (task) {
+$scope.editingTask = task;
+$scope.editedTaskName = task.name;
+};
+$scope.saveTask = function () {
+if ($scope.editingTask) {
+$scope.editingTask.name = $scope.editedTaskName;
+$scope.cancelEdit();
+}
+};
+$scope.cancelEdit = function () {
+$scope.editingTask = null;
+$scope.editedTaskName = '';
+12};
+$scope.deleteTask = function (task) {
+var index = $scope.tasks.indexOf(task);
+if (index !== -1) {
+$scope.tasks.splice(index, 1);
+}
+};
+});
